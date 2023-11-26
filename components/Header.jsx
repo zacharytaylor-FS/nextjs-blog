d4b79d68e6a61eb59ffd1d77c6249922f64f0bae
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-11-25 21:36:42 -0500

    Header updated with NextUI, Footer, PostDetail, and Theme toggle

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,20 +4,21 @@ import Image from "next/image";
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, Input} from "@nextui-org/react";
 import Link from "next/link";
 
+const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
 
 const Header = () => {
-    const menuItems = [
-        "Profile",
-        "Dashboard",
-        "Activity",
-        "Analytics",
-        "System",
-        "Deployments",
-        "My Settings",
-        "Team Settings",
-        "Help & Feedback",
-        "Log Out",
-      ];
+    const items = menuItems.map(i => <li>{i}</li>)
     return (
         <header className="w-full">
             <Navbar isBordered className="flex items-center justify-between p-2 m-2 text-gray-600 dark:text-gray-400">
--- components/PostDetails.jsx
+++ components/PostDetails.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
-import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/react";
+Link
+import {Card, CardHeader, CardBody, CardFooter, Divider, Image} from "@nextui-org/react";
+import Link from 'next/link';
 
 const PostDetail = () => {
     return (
-        <div>
-            postdetails
+        <div className=''>
+
+        
+        <Card className='w-[450px] min-w-[380px]'>
+        <CardHeader className='flex gap-3'>
+            <Image
+          alt="nextui logo"
+          height={40}
+          radius="sm"
+          src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+          width={40}
+        />
+            <div className='flex flex-col'>
+                <p className='text-md'>NextUI</p>
+                <Link isExternal href='https://nextui.org' className='text-small'>nextui.org</Link>
+            </div>
+        </CardHeader>
+        <Divider />
+            <CardBody>
+                <p>Post details</p>
+            </CardBody>
+        </Card>
         </div>
     )
 }
