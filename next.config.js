e5b5e1c08ea3f887089fa22cd4913fbf7b2dd0fc
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-12-26 06:15:03 -0500

    install sanity vercel deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
     images: {
         remotePatterns: [
           {
@@ -10,7 +11,28 @@ const nextConfig = {
       },
       experimental: {
         taint: true
-      }
+      },
+      // exportPathMap: async function (defaultPathMap) {
+  
+      //   const path = await client
+      //     // get all the posts and return those with slugs
+      //     .fetch('*[_type == "post"].slug.current')
+      //     .then(data =>
+      //       // use reduce to build an object with routes
+      //       // and select the blog.js file, and send in the
+      //       // correct query paramater.
+      //       data.reduce(
+      //         (acc, slug) => ({
+      //           '/': { page: '/' },
+      //           ...acc,
+      //           [`/blog/${slug}`]: { page: '/blog', query: { slug } }
+      //         }),
+      //         {}
+      //       )
+      //     )
+      //     .catch(console.error)
+      //   return path
+      // }
 }
 
 module.exports = nextConfig
